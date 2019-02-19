e99baacc32a04a3ca17201c128634c51ce26ff78
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-02-18 16:04:38 -0800

    added gallery page

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-botstrap';
+import Gallery from './Gallery';
 
-class App extends Component{
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      query: '',
+      items: []
+    }
+  }
+
+  search(){
+    const BASE_URL = 'https://www.googleapis.combooks/v1/volumes?q=';
+    fetch(`${BASE_URL}${this.state.query}`, {method: 'GET'} )
+    .then(response => response.json())
+    .then(json => {
+        let { items } = json;
+        this.setState({items })
+    });
+  }
+
+    // console.log('search', this.state.query);
   
   render(){
     return(
-      <div> 
+      <div className='App'> 
         <h2>Book Wormer</h2>
         <FormGroup>
-          <FormControl type= 'text' placeholder='search for a book' />
-            <Glyphicon glyph='search'></Glyphicon>
+          <FormControl 
+            type= 'text' 
+            placeholder='search for a book' 
+            onChange={event => this.setState({query: event.target.value})}
+            onKeyPress={event => {
+              if(event.key === 'Enter'){ this.search }
+            }}
+          />
+          <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph='search'></Glyphicon>
+          </InputGroup.Addon>
         </FormGroup>
+        <Gallery items={this.state.items}/>
       </div>
     )
   }
